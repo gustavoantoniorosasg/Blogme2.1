eff2588dd9e2283a8a53d83c01c8d2d7e0d166ee
Gustavo <gustavoantonio.rosasg@cbtis.edu.mx>
2025-12-03 21:50:16 -0600

    Inicial proyecto Blogme2.1

--- frontend/src/js/publicaciones.js
+++ frontend/src/js/publicaciones.js
@@ -6,7 +6,7 @@
    - Integración con comments.js via window.openComments(postId)
    - Optimistic UI y parcheo (solo actualiza HTML necesario)
 */
-import { API_ADMIN, API_USUARIOS, API_PUBLICACIONES } from "./config.js";
+
 
 (() => {
   'use strict';
